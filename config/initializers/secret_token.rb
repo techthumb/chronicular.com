564b2cc8ae22d5f0406ac4f1ef1068ecd65a05b6
Kunal Parikh <kunal@techthumb.in>
2011-10-04 10:33:57 +1100

    base rails 3.1 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChronicularCom::Application.config.secret_token = 'b784a9a54d0b833828ccbffc184594643e93fc873432880cd4597ebc3ac155f090e8ebee2cb6c3a153279537d272dc88bce722d7fd092a5337839dad284699d0'
